eda12e2e1622def253b669f5e841fe61e3ad72d9
dalan <darancode@gmail.com>
2025-05-23 12:15:16 +0900

    [BE8]김달원

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,83 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("== 명언 앱 ==");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        int cnt = 0; // 명언 번호를 위한 counting
+        List<String[]> index = new ArrayList<>();
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine().trim();
+
+            if (cmd.equals("종료")) {
+                break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String wiseSayingContent = scanner.nextLine().trim();
+                System.out.print("작가 : ");
+                String wiseSayingAuthor = scanner.nextLine().trim();
+
+                String[] entry = new String[2];
+                entry[0] = wiseSayingContent;  // 명언
+                entry[1] = wiseSayingAuthor;   // 작가
+                // 리스트에 추가
+                index.add(entry);
+
+                System.out.println( (cnt + 1) + "번 명언이 등록되었습니다.");
+                cnt++;
+            } else if (cmd.equals("목록")) {
+                if(cnt > 0) {
+                    for (int i = cnt;i > 0;i--) {
+                        System.out.println(i + " / " + index.get(i-1)[0] + " / " + index.get(i-1)[1]);
+                    }
+                }
+            } else if (cmd.startsWith("삭제?id=")) {
+
+                // id= 이후의 숫자를 추출
+                int id = Integer.parseInt(cmd.substring("삭제?id=".length()));
+
+                if (id >= 1 && id <= index.size()) {
+                    index.remove(id - 1);
+                    cnt--;
+                    System.out.println(id + "번 명언이 삭제되었습니다.");
+                } else {
+                    System.out.println(id + "번 명언은 존재하지 않습니다.");
+                }
+            }else if (cmd.startsWith("수정?id=")) {
+
+                // id= 이후의 숫자를 추출
+                int id = Integer.parseInt(cmd.substring("수정?id=".length()));
+                System.out.println("수정id길이" + cmd.substring("수정?id=".length()));
+                System.out.println("index.size = " + index.size());
+                if (id >= 1 && id <= index.size()) {
+
+                    System.out.println("명언(기존) : " + index.get(id - 1)[0]);
+                    System.out.print("명언 : ");
+                    String wiseSayingContent = scanner.nextLine().trim();
+                    index.get(id - 1)[0] = wiseSayingContent;
+
+                    System.out.println("작가(기존) : " + index.get(id - 1)[1]);
+                    System.out.print("작가 : ");
+                    String wiseSayingAuthor = scanner.nextLine().trim();
+                    index.get(id - 1)[1] = wiseSayingAuthor;
+
+
+                } else {
+                    System.out.println(id + "번 명언은 존재하지 않습니다.");
+                }
+            }
         }
+
+
+
+
+        scanner.close();
     }
 }
